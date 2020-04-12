227665db9dca3c821e8d69cb2459728672b46ca4
Tahoe <bonjour@tahoe.be>
2020-04-12 18:13:39 +0200

    Optimisation.
    · même fonction pour tout calculer
    · retour du troisième calcul
    · j'ai mis une fonction en argument d'une autre.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,26 +1,10 @@
 id = name => document.getElementById(name);
 cl = name => document.getElementsByClassName(name);
 
-let inputs = document.querySelectorAll(".input");
-let allowed = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", ",", "."];
-
-
 // Pour remplacer les virgules par des points
 function comReplace(value) {
 	return value.replace(",", ".");
 }
-
-// Pour savoir combien y a de décimales
-Number.prototype.countDecimals = function () {
-    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
-    return this.toString().split(".")[1].length || 0; 
-}
-
-
-
-
-
-
 
 // Indication d'erreur quand mauvaise touche
 function error(input) {
@@ -29,10 +13,12 @@ function error(input) {
 	setTimeout(() => { input.classList.remove("error"); }, 150);
 }
 
-function filterCalc(input) {
+function launcher(input) {
 
 	// filtre les inputs
 	input.addEventListener('keypress', function(e) {
+		const allowed = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", ",", "."];
+
 		if (!allowed.includes(e.key)) {
 			error(input);
 		//truc compliqué pour empêcher d'avoir plusieurs virgules
@@ -43,21 +29,69 @@ function filterCalc(input) {
 
 	// à chaque entrée de texte
 	input.addEventListener('input', function(e) {
-
-		// indique si les inputs sont pleins/vides
+		// indique si les inputs sont pleins/vides (pour  le % de la bonne couleur)
 		if (input.innerText.length > 0) {
 			input.classList.add("full");
 		} else {
 			input.classList.remove("full");
 		}
 
-		// lance les calculs
-		calculSoldes();
-		calculQuantite()
+		// lance le calculs
+		processor(input);
 	});
 }
 
-inputs.forEach(filterCalc);
+function processor(input) {
+	// Récupère le parent pour savoir à qui on a affaire
+	let parent = input.parentNode;
+
+	let x = comReplace(parent.querySelector("#x").innerText);
+	let y = comReplace(parent.querySelector("#y").innerText);
+
+	let zInput = parent.querySelector("#resultat");
+
+	if (parent.classList.contains("solde")) {
+		// si les deux inputs sont pleins
+		if (x && y) {
+			// lance le calcul puis le traite
+			processResult(calcul("solde", x, y), false, zInput);
+		} else {
+			// reset quand les inputs sont vides
+			reset(zInput);
+		}
+	} else if (parent.classList.contains("quant")) {
+		if (x && y) {
+			processResult(calcul("quant", x, y), true, zInput);
+		} else {
+			reset(zInput);
+		}
+	} else if (parent.classList.contains("evo")) {
+		if (x && y) {
+			processResult(calcul("evo", x, y), false, zInput);
+		} else {
+			reset(zInput);
+		}
+	}
+}
+
+// Limite les décimales à 6, inscrit le résultat et ajoute la class .valid
+function processResult(value, isPercent, resultInput) {
+	let result = parseFloat(value.toFixed(6));
+
+	if (isPercent) {
+		resultInput.innerText = result + "%";
+	} else {
+		resultInput.innerText = result;
+	}
+
+	resultInput.classList.add("valid");
+}
+
+// Pour savoir combien y a de décimales
+Number.prototype.countDecimals = function () {
+    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
+    return this.toString().split(".")[1].length || 0; 
+}
 
 function reset(toReset) {
 	// removes previous result
@@ -65,91 +99,17 @@ function reset(toReset) {
 	toReset.classList.remove("valid");
 }
 
-
-
-function calculSoldes() {
-	// prend les valeurs et remplace , par .
-	let pourcent = comReplace(id("pourcent").innerText);
-	let total = comReplace(id("total").innerText);
-
-	let reduced = id("reduced");
-
-	if (pourcent && total) {
-		// calcule
-		let reducedVal = total * (pourcent / 100);
-
-		// limite à 6 décimales
-		smallerResult(reducedVal, false, reduced);
-
-	} else {
-		reset(reduced);
-	}
-}
-
-function calculQuantite() {
-	let quantite = comReplace(id("quantite").innerText);
-	let quantiteTotale = comReplace(id("quantiteTotale").innerText);
-
-	let quantiteResult = id("quantiteResult");
-
-	if (quantite && quantiteTotale) {
-		// calcule
-		let quantiteResultVal = (100 * quantite) / quantiteTotale;
-		
-		// limite à 6 décimales
-		smallerResult(quantiteResultVal, true, quantiteResult);
-
-		
-	} else {
-		// removes previous result
-		reset(quantiteResult);
-	}
-}
-
-
-
+// des belles mathématiques
 function calcul(type,x,y) {
-	if (type === "solde") {
-		return y * (x / 100);
-	} else if (type === "quant") {
-		return (100 * x) / y;
-	} else if  (type === "evo") {
-		return ((y - x) / x) * 100;
+	switch(type) {
+		case "solde":
+			return y * (x / 100);
+		case "quant":
+			return (100 * x) / y;
+		case "evo":
+			return ((y - x) / x) * 100;
 	}
 }
 
-// Limite les décimales à 6, ajoute la class valid et inscrit le résultat
-function smallerResult(value, isPercent, resultInput) {
-	let smaller = parseFloat(value.toFixed(6));
-
-	if (isPercent) {
-		resultInput.innerText = smaller + "%";
-	} else {
-		resultInput.innerText = smaller;
-	}
-
-	resultInput.classList.add("valid");
-}
-
-
-
-
-
-// function thirdCalc() {
-// 	let startValue = (document.getElementById("startValue").innerText).replace(",", ".");
-// 	let endValue = (document.getElementById("endValue").innerText).replace(",", ".");
-// 	let thirdResult = document.getElementById("rate");
-
-// 	if (startValue && endValue) {
-// 		let rate = ((endValue - startValue) / startValue) * 100;
-
-// 		rate = rate.toFixed(6);
-// 		rate = parseFloat(rate);
-
-// 		thirdResult.innerText = rate;
-// 		thirdResult.classList.add("valid");
-// 	} else {
-// 		thirdResult.innerText = "\u00A0";
-// 		thirdResult.classList.remove("valid");
-// 	}
-// }
+let inputs = document.querySelectorAll(".input");
+inputs.forEach(launcher);
