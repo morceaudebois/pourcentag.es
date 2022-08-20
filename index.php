0b59aa85a4d2303c8c85d4cef1f995d31439d0af
Tahoe <bonjour@tahoe.be>
2022-08-20 11:37:24 +0200

    Finalised automatic PHP translation
    also yeet google analytics

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
+<?php
+	$language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+	putenv("LANG=$language");
+	setlocale(LC_ALL, $language);
+
+	$domain = "messages";
+
+	bindtextdomain($domain, "locales");
+	bind_textdomain_codeset($domain, 'UTF-8');
+
+	textdomain($domain);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	
-	<title><?= _('Calculate percentages'); ?> · pourcentag.es</title>
-	<meta name="description" content="<?php _(`Calculate percentages in a minimalist modern interface. Use common phrases to add a percentage, calculate values and evolutions.`); ?>" />
+
+	<title><?= _('Calculate percentages') ?> · pourcentag.es</title>
+	<meta name="description" content="<?= _('Calculate percentages in a minimalist modern interface. Use common phrases to add a percentage, calculate values and evolutions.') ?>" />
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	
@@ -24,30 +38,19 @@
     <meta name="apple-mobile-web-app-title" content="pourcentag.es">
     <meta name="apple-mobile-web-app-status-bar-style" content="white">
 	
-    <meta property="og:title" content="pourcentag.es · <?= _('Modern percentage calculator') ?>">
+    <meta property="og:title" content="pourcentag.es · <?= _('Simple and efficient percentage calculator') ?>">
     <meta property="og:description" content="<?= _('Calculate percentages in a minimalist modern interface. Use common phrases to add a percentage, calculate values and evolutions.') ?>">
     <meta property="og:image" content="https://pourcentag.es/src/images/preview.jpg">
     <meta property="og:type" content="website"/>
 
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:description" content="<?= _('Calculate percentages in a minimalist modern interface. Use common phrases to add a percentage, calculate values and evolutions.') ?>">
-    <meta property="twitter:title" content="pourcentag.es · <?= _('Modern percentage calculator') ?>">
+    <meta property="twitter:title" content="pourcentag.es · <?= _('Simple and efficient percentage calculator') ?>">
 	<meta property="twitter:image" content="https://pourcentag.es/src/images/preview.jpg">
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136684433-4"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', 'UA-136684433-4');
-	</script>
-
 </head>
 
 <body>
-	<?php include "counter.php"; ?>
+	<?php include "counter.php" ?>
 
 	<picture id="logo">
 		<source srcset="src/images/nightlogo.svg" media="(prefers-color-scheme: dark)" />
@@ -126,11 +129,11 @@
 		<p><?= _('You\'re visitor n°') ?></p>
 		<div class="compteur"><span><?= $count ?></span></div>
 
-		<p><?= _('pourcentag.es is a') ?> <a href="https://dribbble.com/tags/neomorphism" target="_blank" ><?= _('neumorphic') ?></a> <?= _('experiment, a combination of interfaces from the') ?> <a href="https://fr.wikipedia.org/wiki/Skeuomorphisme" target="_blank"><?= _('past') ?></a> <?= _('and the') ?> <a href="https://fr.wikipedia.org/wiki/Flat_design" target="_blank"><?= _('present') ?></a>.</p>
+		<p><?= _('pourcentag.es is a') ?> <a href="https://dribbble.com/tags/neomorphism" target="_blank" ><?= _('neumorphic ') ?></a><?= _('experiment, a combination of interfaces from the') ?> <a href="https://fr.wikipedia.org/wiki/Skeuomorphisme" target="_blank"><?= _('past') ?></a> <?= _('and the') ?> <a href="https://fr.wikipedia.org/wiki/Flat_design" target="_blank"><?= _('present') ?></a>.</p>
 
 		<p><?= _('If you like the idea or find it useful, you can') ?> <a href="https://www.buymeacoffee.com/Tahoe" target="_blank"><?= _('buy me a coffee') ?></a>. ☕️</p>
 
-		<a href="https://github.com/Tahoooe/pourcentage" id="link" target="_blank">
+		<a href="https://github.com/morceaudebois/pourcentage" id="link" target="_blank">
 			<svg width="18" height="18" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" transform="scale(64)"/>
 			</svg>
