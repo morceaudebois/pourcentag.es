6b3d28ff8529e247d6d744b038d9972d923cdd29
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-02-15 15:44:16 +0100

    Logo to png + SEO
    SVG logo had a weird antialiasing issue

--- index.php
+++ index.php
@@ -53,10 +53,7 @@
 <body>
 	<?php include "counter.php" ?>
 
-	<picture id="logo" width="600" height="89" >
-		<source srcset="src/images/nightlogo.svg" media="(prefers-color-scheme: dark)" />
-		<img src="src/images/logo.svg"  alt="logo" width="600" height="89" draggable="false">
-	</picture>
+	<img id="logo" width="600" height="89" src="src/images/logo.png"  alt="logo" draggable="false">
 
 	<h1><?= _("Simple and efficient percentage calculator") ?></h1>
 
@@ -130,11 +127,11 @@
 		<p><?= _("You're visitor n°") ?></p>
 		<div class="compteur"><span><?= $count ?></span></div>
 
-		<p><?= _("pourcentag.es is a") ?> <a href="https://dribbble.com/tags/neomorphism" target="_blank" ><?= _("neumorphic ") ?></a><?= _("experiment, a combination of interfaces from the") ?> <a href="https://fr.wikipedia.org/wiki/Skeuomorphisme" target="_blank"><?= _("past") ?></a> <?= _("and the") ?> <a href="https://fr.wikipedia.org/wiki/Flat_design" target="_blank"><?= _("present") ?></a>.</p>
+		<p><?= _("pourcentag.es is a") ?> <a href="https://dribbble.com/tags/neomorphism" target="_blank" title="Check out neomorphism on Dribbble"><?= _("neumorphic ") ?></a><?= _("experiment, a combination of interfaces from the") ?> <a href="https://en.wikipedia.org/wiki/Skeuomorph" target="_blank" title="check out skeuomorphism on Wikipedia"><?= _("past") ?></a> <?= _("and the") ?> <a href="https://fr.wikipedia.org/wiki/Flat_design" target="_blank" title="check out flat design on Wikipedia"><?= _("present") ?></a>.</p>
 
-		<p><?= _("If you like the idea or find it useful, you can") ?> <a href="https://ko-fi.com/tahoe" target="_blank"><?= _("buy me a coffee") ?></a>. ☕️</p>
+		<p><?= _("If you like the idea or find it useful, you can") ?> <a href="https://ko-fi.com/tahoe" target="_blank" title="Donate to author on Ko-Fi"><?= _("buy me a coffee") ?></a>. ☕️</p>
 
-		<a href="https://github.com/morceaudebois/pourcentag.es" id="githubLink" target="_blank" aria-label="pourcentag.es github repository">
+		<a href="https://github.com/morceaudebois/pourcentag.es" id="githubLink" target="_blank" aria-label="pourcentag.es github repository" title="check out the source code on GitHub">
 			<svg width="18" height="18" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" transform="scale(64)"/>
 			</svg>
@@ -148,7 +145,7 @@
 
 		<h3>⚠️ <?= _("You don't wanna read this.") ?> ⚠️</h3>
 
-		<p><i><?= _("Small websites like these won't get referenced well by Google unless they have tons of text and keywords. I'm not really into those since pourcentag.es is designed to be easy to use and not get in your way. Here's some AI generated text and images you shouldn't read but might please the SEO gods. Please hide it using the big red button and it won't appear anymore.") ?></i></p>
+		<p><i><?= _("Small websites like these won't get referenced well by Google unless they have tons of text and keywords. I'm not really into those since pourcentag.es is designed to be easy to use and not get in your way. Here's some AI generated text and images you shouldn't read but might please the SEO gods. Please hide it using the big red button and it will happily disappear") ?></i></p>
 
 		<button class="pushable">
 			<span class="shadow"></span>
